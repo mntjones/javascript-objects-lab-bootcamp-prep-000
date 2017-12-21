ee2909074fdf52de739b05be94163c3d91b36152
Monica Tamaru Jones <mntamaru@gmail.com>
2017-12-21 17:32:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 
 function deleteFromObjectByKey(obj, key) {
   var newObj = Object.assign({}, obj)
-  delete newObj.key
+  delete newObj[key]
   return newObj
 }
 
