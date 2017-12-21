d97adf92e6a51147d84a9eaf20f67332bbbb1f84
Monica Tamaru Jones <mntamaru@gmail.com>
2017-12-21 17:25:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-  
+  return Object.assign({}, obj, { [key]: value })
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
