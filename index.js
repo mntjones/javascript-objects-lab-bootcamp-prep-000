df93ce00de0a405c16cc42c21044207d6ebd93f7
Monica Tamaru Jones <mntamaru@gmail.com>
2017-12-21 17:06:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  
+}
+
+function deleteFromObjectByKey(obj, key) {
+  
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  
+}
